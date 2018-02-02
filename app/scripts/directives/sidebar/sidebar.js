154f03d904ae8c5c9ad8b54934612708ae21010b
siddeshujjni <sidd335@gmail.com>
2018-02-02 12:36:21 -0500

    Add files via upload

---
+++ app/scripts/directives/sidebar/sidebar.js
@@ -0,0 +1,48 @@
+/**
+ * @ngdoc directive
+ * @name izzyposWebApp.directive:adminPosHeader
+ * @description
+ * # adminPosHeader
+ */
+
+angular.module('sbAdminApp')
+  .directive('sidebar',['$location',function() {
+    return {
+      templateUrl:'scripts/directives/sidebar/sidebar.html',
+      restrict: 'E',
+      replace: true,
+      scope: {
+      },
+      controller:function($scope){
+        $scope.selectedMenu = 'dashboard';
+        $scope.collapseVar = 0;
+        $scope.multiCollapseVar = 0;
+        $scope.isAdmin = localStorage.isAdmin;
+        $scope.chosen = localStorage.chosen;
+        $scope.courseRole = localStorage.role;
+        
+        $scope.check = function(x){
+
+          $scope.chosen = localStorage.chosen;
+          
+          if(x==$scope.collapseVar)
+            $scope.collapseVar = 0;
+          else
+            $scope.collapseVar = x;
+        };
+        
+        $scope.multiCheck = function(y){
+
+          $scope.chosen = localStorage.chosen;
+          
+          if(y==$scope.multiCollapseVar)
+            $scope.multiCollapseVar = 0;
+          else
+            $scope.multiCollapseVar = y;
+        };
+
+        
+
+      }
+    }
+}]);
